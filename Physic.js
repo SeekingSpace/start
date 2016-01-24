f42c9b3f53679be606509440ee6adec60b38055b
SerchForVideos <serchnh17@hotmail.com>
2016-01-24 03:45:15 -0600

    New Additions
    Work is still going.

--- Physic.js
+++ Physic.js
@@ -1,6 +1,22 @@
-function runPhysic() {
-        var obj=document.getElementsByTagName("svg");
-        console.log(obj);
-        var obj=document.getElementsByClassName("SpaceObject");
-        console.log(obj);
+function squareDistance(x1,y1,x2,y2) {
+    return (x1-x2)**2+(y1-y2)**2;
+}
+
+function force(object1,object2){
+    return (object1.mass*object2.mass)/squareDistance(object1.x,object1.y,object2.x,object2.y);
+}
+
+function globalForce(testedObject,objects){
+    var totalForce=0;
+    for (var i = 0; i < objects.length; i++) {
+
+    }
+    return [*];
+}
+
+function runPhysic(objects) {
+        console.log("done!");
+        for (var i = 0; i < objects.length; i++) {
+            globalForce(objects[i],objects)
+        }
     }
--- SpaceObject.js
+++ SpaceObject.js
@@ -1,18 +1,6 @@
 //class SpaceObject {
 //var mass=0,x=0,y=0,velocity=0,direction=0/*degrees*/;
 //var volume=0,radiusValue=0;
-/*constructor(x,y,mass){
-    this.mass=mass;
-    this.x=x;
-    this.y=y;
-}
-constructor(x,y,mass,velocity,direction) {
-    this.mass=mass;
-    this.x=x;
-    this.y=y;
-    this.velocity=velocity;
-    this.direction=direction;
-}*/
 function setMass(mass){
     this.mass=mass;
 }
@@ -32,15 +20,14 @@ function setVector(direction,velocity){
 }
 function setDisplay(){
     var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    c.setAttribute('cx',51);
-    c.setAttribute('cy',51);
-    c.setAttribute('r',40);
+    c.setAttribute('cx',this.x);
+    c.setAttribute('cy',this.y);
+    c.setAttribute('r',this.mass/2);
     c.setAttribute('stroke','green');
-    c.setAttribute('stroke-width',4);
+    c.setAttribute('stroke-width',this.mass/100);
     c.setAttribute('fill','yellow');
     var col=document.getElementsByTagName("svg")[0]
     col.appendChild(c);
-    console.log(col);
 }
 function SpaceObject(x,y,mass){
     this.setMass=setMass;
