52441978612d4aefe592612a9b47798912ed33f5
SerchForVideos <serchnh17@hotmail.com>
2016-03-17 01:34:29 -0600

    Hard work and free time
    I finally implemented in the correct way the physics (1)!

--- Physic.js
+++ Physic.js
@@ -1,83 +1,51 @@
-//This needs work!
-function toRadians(x){
-    return x*Math.PI/180;
-}
-function toDegrees(x){
-    return x*180/Math.PI;
-}
-function newX(velocity,angle){
-    return velocity*toDegrees(Math.cos(toRadians(angle)));
-}
-function newY(velocity,angle){
-    return velocity*toDegrees(Math.sin(toRadians(angle)));
+//This needs work 2!
+////////////////////////////////////////////////
+//All PREVIOUS CODE WAS DEPRECATED
+////////////////////////////////////////////////
+function distance(x,y,z){
+    return Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
 }
 
-function squareDistance(x1,y1,x2,y2){
-    return Math.pow(x2-x1,2)+Math.pow(y2-y1,2);
-}
-function distance(x1,y1,x2,y2){
-    return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-}
+function gravityForce(objects){
+    var forceX,forceY,forceZ,totalForce;//force components
+    var x,y,z;//vector coords
+    for(var i = 0; i < objects.length; i++) {//EACH SPACEOBJECT
+        forceX =objects[i].velocityX*objects[i].mass;
+        forceY =objects[i].velocityY*objects[i].mass;
+        forceZ =objects[i].velocityZ*objects[i].mass;
+        for (var j = 0; j < objects.length; j++) {//EACH TARGET
+            totalForce=0;
+            x =objects[j].x-objects[i].x;
+            y =objects[j].y-objects[i].y;
+            z =objects[j].z-objects[i].z;
 
-function force(mass1,mass2,x1,y1,x2,y2){
-    if (x2-x1==0 && y2-y1==0){
-        return 0;
-    } else {
-        //console.log((mass1*mass2)/squareDistance(x1,y1,x2,y2));
-        return (mass1*mass2)/squareDistance(x1,y1,x2,y2);
+            xyzdistance=distance(x,y,z);
+            if(xyzdistance==0){xyzdistance=1};
+            //console.log(xyzdistance);
+            totalForce =objects[i].mass*objects[j].mass/xyzdistance;
+            //console.log(totalForce);
+            forceX     =totalForce*x/xyzdistance+forceX;
+            forceY     =totalForce*y/xyzdistance+forceY;
+            forceZ     =totalForce*z/xyzdistance+forceZ;
+        }
+        objects[i].velocityX =forceX/objects[i].mass;
+        objects[i].velocityY =forceY/objects[i].mass;
+        objects[i].velocityZ =forceZ/objects[i].mass;
     }
-}
-
-function angleToObject(x1,y1,x2,y2){
-    if (x2-x1==0 && y2-y1==0){
-        return 0;
-    } else {
-        //console.log(Math.asin((y2-y1)/distance(x1,y1,x2,y2)));
-        wait=toDegrees(Math.asin(toRadians((y2-y1)/distance(x1,y1,x2,y2))));
-        console.log(wait+" degrees")
-        if (wait<0) {wait=360+wait;}
-        return wait;
+    for (var n = 0; n < objects.length; n++) {
+        //console.log(objects[n].x);
+        objects[n].x+=objects[n].velocityX;
+        objects[n].y+=objects[n].velocityY;
+        objects[n].z+=objects[n].velocityZ;
+        //console.log(objects[n].x);
+        objects[n].setDisplay();
     }
-}
-
-function addForces(force1,force2,angle1,angle2){
-    console.log("forces added "+force1,force2,angle1,angle2);
-    var sins=force1*toDegrees(Math.sin(toRadians(angle1)))+force2*toDegrees(Math.sin(toRadians(angle2)));
-    console.log("sins "+sins);
-    var coss=force1*toDegrees(Math.cos(toRadians(angle1)))+force2*toDegrees(Math.cos(toRadians(angle2)));
-    console.log("coss "+coss);
-    var resultingForce=(sins^2+coss^2)^(1/2);
-    var resultingAngle=toDegrees(Math.atan2(toRadians(sins),toRadians(coss)));
-    if (resultingAngle<0) {resultingAngle=360+resultingAngle;}
-    return [resultingForce,resultingAngle];
-}
-
-function globalForce(target,objects){
-    var totalForce=target.velocity*target.mass,targetAngle=target.angle,i,forceCatch=[];
-    for (i in objects){
-        var objectForce=force(target.mass,objects[i].mass,target.x,target.y,objects[i].x,objects[i].y);
-        var objectAngle=angleToObject(target.x,target.y,objects[i].x,objects[i].y);
-        forceCatch=addForces(totalForce,objectForce,targetAngle,objectAngle);
-        console.log("force and angle "+forceCatch);
-        target.angle=forceCatch[1];
-        target.velocity=forceCatch[0]/target.mass;
-    }
-    return target;
 }
 
 function runPhysic(objects){
-    console.log(objects);
-    var newObjects=[];
-    for (j in objects){
-        newObjects.push(globalForce(objects[j],objects));
-    }
-    for (k in newObjects){
-        objects[k].x=objects[k].x+newX(objects[k].velocity,objects[k].angle);
-        objects[k].y=objects[k].y+newY(objects[k].velocity,objects[k].angle);
-        objects[k].svg.setAttribute('cx',objects[k].x);
-        objects[k].svg.setAttribute('cy',objects[k].y);
-    }
-    console.log(newObjects);
+    gravityForce(objects);
+
+
+    //console.log(newObjects);
     console.log("done!");
-    return newObjects;
 }
--- SpaceObject.js
+++ SpaceObject.js
@@ -1,42 +1,30 @@
 //class SpaceObject {
 //var mass=0,x=0,y=0,velocity=0,angle=0/*degrees*/;
 //var volume=0,radiusValue=0;
-function setMass(mass){
-    this.mass=mass;
-}
-function setXCenter(x){
-    this.x=x;
-}
-function setYCenter(y){
-    this.y=y;
-}
-function setCenterCoordinates(x,y){
-    this.x=x;
-    this.y=y;
-}
 function setDisplay(){
-    var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    c.setAttribute('cx',this.x);
-    c.setAttribute('cy',this.y);
-    c.setAttribute('r',this.mass/2);
-    c.setAttribute('stroke','green');
-    c.setAttribute('stroke-width',this.mass/100);
-    c.setAttribute('fill','yellow');
-    var col=document.getElementsByTagName("svg")[0];
-    col.appendChild(c);
-    this.svg=c;
-}
-function SpaceObject(x,y,mass){
-    this.setMass=setMass;
-    this.setXCenter=setXCenter;
-    this.setYCenter=setYCenter;
-    this.setCenterCoordinates=setCenterCoordinates;
-    this.setDisplay=setDisplay;
+    this.image.setAttribute('cx',this.x);
+    this.image.setAttribute('cy',this.y);
+    this.image.setAttribute('r',this.mass/(5));
+    this.image.setAttribute('stroke','green');
+    this.image.setAttribute('stroke-width',this.mass/100);
+    this.image.setAttribute('fill','yellow');
 
-    this.velocity=0;
-    this.angle=0;
+}
+function SpaceObject(x,y,z,mass){
+    ////properties
     this.mass=mass;
+    this.velocityX=0;
+    this.velocityY=0;
+    this.velocityZ=0;
     this.x=x;
     this.y=y;
-    this.svg;
+    this.z=z;
+
+
+    ////graphics
+    this.setDisplay=setDisplay;
+    this.svg = document.getElementsByTagName("svg")[0];
+    this.image = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    this.setDisplay();
+    this.svg.appendChild(this.image);
 }
